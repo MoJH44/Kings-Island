cbf055c4ae8c94e1cd3e646504be64e705c65554
MoJH44 <37304200+MoJH44@users.noreply.github.com>
2018-11-01 00:34:24 -0400

    pep to people

--- Kingsisland.java
+++ Kingsisland.java
@@ -6,14 +6,14 @@ public class Kingsisland {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int peps;
+		int people;
 		double parking$, food, total$, perpep$, unused;
 		Scanner keyboard = new Scanner(System.in);
 
 		
 
 		System.out.print("How many People ");
-		peps = keyboard.nextInt();
+		people = keyboard.nextInt();
 
 		System.out.print("Parking Amount ");
 		parking$ = keyboard.nextDouble();
@@ -24,7 +24,7 @@ public class Kingsisland {
 
 
 		total$ = (parking$ + food$);
-		perpep$ = (total$/peps);
+		perpep$ = (total$/people);
 		System.out.printf("Your total is " + total$);
 		System.out.printf("  Your Average per Person is " + perpep$ + "  ");
 		
